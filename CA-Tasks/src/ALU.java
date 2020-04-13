68685dc090afaa46cc7ff3a77fb763e56163ef5b
Reem Salah <47977185+reeemsalah@users.noreply.github.com>
2020-04-13 18:43:42 +0200

    ALU class for operations

--- CA-Tasks/src/ALU.java
+++ CA-Tasks/src/ALU.java
@@ -1,30 +1,31 @@
+import java.util.Scanner;
 
 public class ALU {
 
-	private static  boolean zero;
-	private static char []output;
+	private static int zero;
+	private static String output;
 
 	public ALU() {
-		zero = true;
-		output=new char[32];
-		for(int i=0;i<32;i++)
-		{
-			output[i]='0';
+		zero =0;
+		output = "";
+		for (int i = 0; i < 32; i++) {
+			output += "0";
 		}
+		output+="/0";
 	}
 
 	/**
 	 * 
 	 * @return if the output is zero or not
 	 */
-	public static boolean isZero() {
-		zero = (output.equals("0000000000000000000000000000000")) ? true : false;
-		return output.equals("0000000000000000000000000000000");
+	public static int isZero() {
+		zero = (output.substring(0, 32).equals("0000000000000000000000000000000")) ? 1 : 0;
+		return zero;
 	}
-	public static void setZero()
-	{
-		zero = (output.equals("0000000000000000000000000000000")) ? true : false;
-	
+
+	public static void setZero() {
+		zero = (output.substring(0, 32).equals("0000000000000000000000000000000")) ? 1 : 0;
+
 	}
 
 	/**
@@ -32,12 +33,7 @@ public class ALU {
 	 * @return the output of the ALU
 	 */
 	public String getOutput() {
-		String res="";
-		for(int i=0;i<output.length;i++)
-		{
-			res+=output[i]+"";
-		}
-		return res;
+		return output;
 	}
 
 	/**
@@ -65,7 +61,7 @@ public class ALU {
 		case "0110":
 			sub(op1, op2);
 			break;
-		case "1110":
+		case "0111":
 			slt(op1, op2);
 			break;
 		case "1100":
@@ -91,24 +87,32 @@ public class ALU {
 		if (op1.length() != 32 && op2.length() != 32) {
 			System.out.println("Inavlid Inputs");
 		} else {
+			int n1 = binToDec(op1);
+			int n2 = binToDec(op2);
 			switch (op) {
 			case "0000":
-				and(op1, op2);
+				System.out.println("Operation Name: AND");
+				and(n1, n2);
 				break;
 			case "0001":
-				or(op1, op2);
+				System.out.println("Operation Name: OR");
+				or(n1, n2);
 				break;
 			case "0010":
-				add(op1, op2);
+				System.out.println("Operation Name: ADD");
+				add(n1, n2);
 				break;
 			case "0110":
-				sub(op1, op2);
+				System.out.println("Operation Name: SUB");
+				sub(n1, n2);
 				break;
-			case "1110":
-				slt(op1, op2);
+			case "0111":
+				System.out.println("Operation Name: SLT");
+				slt(n1, n2);
 				break;
 			case "1100":
-				nor(op1, op2);
+				System.out.println("Operation Name: NOR");
+				nor(n1, n2);
 				break;
 			default:
 				System.out.println("Invalid Operation Code");
@@ -117,59 +121,139 @@ public class ALU {
 		}
 	}
 
-	private static void nor(String op1, String op2) {
-		for(int i=0;i<op1.length()&&i<op2.length();i++)
-		{
-			char c1=op1.charAt(i);
-			char c2=op2.charAt(i);
-			char c=(char)(1-(c1|c2));
-			output[i]=c;
-			
-		}
-	}
-
-	private static void slt(String op1, String op2) {
-
-	}
-
-	private static void sub(String op1, String op2) {
-
-	}
-
-	private static void add(String op1, String op2) {
-
-	}
-
-	private static void or(String op1, String op2) {
-
-	}
-
-	private static void and(String op1, String op2) {
-
-	}
-
 	private static void nor(int op1, int op2) {
+		int res1 = ~(op1 | op2);
+		String res2 = decToBin(res1);
+		output = res2 + "/" + res1;
+		isZero();
 
 	}
 
 	private static void slt(int op1, int op2) {
+		int res1 = (op1 < op2) ? 1 : 0;
+		String res2 = decToBin(res1);
+		output = res2 + "/" + res1;
+		isZero();
 
 	}
 
 	private static void sub(int op1, int op2) {
+		int res1 = op1 - op2;
+		String res2 = decToBin(res1);
+		output = res2 + "/" + res1;
+		isZero();
 
 	}
 
 	private static void add(int op1, int op2) {
+		int res1 = op1 + op2;
+		String res2 = decToBin(res1);
+		output = res2 + "/" + res1;
+		isZero();
 
 	}
 
 	private static void or(int op1, int op2) {
+		int res1 = (op1 | op2);
+		String res2 = decToBin(res1);
+		output = res2 + "/" + res1;
+		isZero();
 
 	}
 
 	private static void and(int op1, int op2) {
+		int res1 = (op1 & op2);
+		String res2 = decToBin(res1);
+		output = res2 + "/" + res1;
+		isZero();
 
+	}
+
+	public static int binToDec(String s) {
+		int res = 0;
+		char sign = s.charAt(1);
+		int power = s.length() - 1;
+		for (int i = 1; i < s.length(); i++) {
+
+			res += Integer.parseInt(s.substring(i, i + 1)) * (int) (Math.pow(2, power));
+			power--;
+		}
+
+		return sign == '0' ? res : (res * -1);
+	}
+
+	public static String decToBin(int n) {
+		String sign = "";
+		if (n < 0) {
+			sign = "1";
+			n=n*-1;
+		}
+		else
+			sign = "0";
+
+		String res = "";
+		while (n != 0) {
+			int d = n % 2;
+			n = n / 2;
+			res = d + res;
+
+		}
+		
+		while(res.length()!=32)
+			res="0"+res;
+		if(sign=="1") {
+			String temp="";
+			int c=0;
+			int i;
+			for(i=31;i>=0;i--)
+			{
+				temp=res.charAt(31)+""+temp;
+				if(res.charAt(i)=='1') 
+					c++;
+				
+				if(c==1) {
+					i--;
+					break;
+				}
+			}
+			if(c==1)
+			{
+				while(i>=0)
+				{
+					if(res.charAt(i)=='1')
+						temp="0"+temp;
+					else
+						temp="1"+temp;
+					i--;
+				}
+			}
+			res=temp;
+		}
+		
+		return res ;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("1st Operand: ");
+		String tmp1=sc.nextLine();
+		System.out.print("2nd Operand: ");
+		String tmp2=sc.nextLine();
+		System.out.print("Operation: ");
+		String op=sc.nextLine();
+		String [] op1=tmp1.split("/");
+		String [] op2=tmp2.split("/");
+		String s1=op1[0];
+		String s2=op2[0];
+		int n1=Integer.parseInt(op1[1]);
+		int n2=Integer.parseInt(op2[1]);
+		ALUEvaluator(op, n1, n2);
+		System.out.println("1st Operand: "+tmp1);
+		System.out.println("2nd Operand: "+tmp2);
+		System.out.println("Output: "+output);
+		System.out.println("Z-flag Value: "+isZero());
+		
+		
+		
 	}
 
 }
